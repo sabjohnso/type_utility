95553a8e7dc1f01d72bc02385cdba8848b231b5f
Samuel B. Johnson <sabjonso@yahoo.com>
2019-08-30 15:51:49 -0500

    Removed unused cmake file

--- cmake/type_utility_deps.cmake
+++ cmake/type_utility_deps.cmake
@@ -3,3 +3,5 @@ find_package(Git REQUIRED)
 execute_process(
   COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive
   WORKING_DIRECTORY ${TYPE_UTILITY_SOURCE_DIR})
+
+list(INSERT CMAKE_MODULE_PATH 0 ${TYPE_UTILITY_SOURCE_DIR}/cmake_modules)
