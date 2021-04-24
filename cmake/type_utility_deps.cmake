a6713ad1dbbf33c2f5064cbd40fdef7f501f1ec8
Samuel B. Johnson <sabjohnso@yahoo.com>
2021-04-24 14:26:33 -0500

    Setup CMake to to cmake_utilities

--- cmake/type_utility_deps.cmake
+++ cmake/type_utility_deps.cmake
@@ -1,9 +1,10 @@
-find_package( Git REQUIRED )
+if(NOT EXISTS ${PROJECT_SOURCE_DIR}/cmake_utilities/FindCMakeUtilities.cmake)
+  find_package( Git REQUIRED )
+  execute_process(
+    COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive
+    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
+endif()
+list(INSERT CMAKE_MODULE_PATH 0 ${TYPE_UTILITY_SOURCE_DIR}/cmake_utilities)
+find_package(CMakeUtilties)
 
-execute_process(
-  COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive
-  WORKING_DIRECTORY ${TYPE_UTILITY_SOURCE_DIR})
-
-list( INSERT CMAKE_MODULE_PATH 0 ${TYPE_UTILITY_SOURCE_DIR}/cmake_modules )
-
-include( FetchNLohmannJSON )
+find_package(nlohmann_json REQUIRED)
