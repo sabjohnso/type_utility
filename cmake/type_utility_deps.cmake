04fd37a471020655e1b12cb73ef6f7f14743f760
Samuel B. Johnson <sabjohnso@yahoo.com>
2021-04-24 15:39:56 -0500

    Modified test cmake to use find_package for GTest

--- cmake/type_utility_deps.cmake
+++ cmake/type_utility_deps.cmake
@@ -4,7 +4,7 @@ if(NOT EXISTS ${PROJECT_SOURCE_DIR}/cmake_utilities/FindCMakeUtilities.cmake)
     COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive
     WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
 endif()
-list(INSERT CMAKE_MODULE_PATH 0 ${TYPE_UTILITY_SOURCE_DIR}/cmake_utilities)
-find_package(CMakeUtilties)
+list(INSERT CMAKE_MODULE_PATH 0 ${PROJECT_SOURCE_DIR}/cmake_utilities)
+find_package(CMakeUtilities)
 
 find_package(nlohmann_json REQUIRED)
